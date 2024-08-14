8cf8190c5eb4844da5597055ee78cb36a39736e8
JK-Darsanaa-18 <124986398+JK-Darsanaa-18@users.noreply.github.com>
2024-08-14 16:00:41 +0530

    Delete backend directory

--- backend/myapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Login, StaffDetails, ShiftForm
-
-admin.site.register(Login)
-admin.site.register(StaffDetails)
-admin.site.register(ShiftForm)
--- backend/myproject/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from myapp.views import home
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('myapp.urls')),
-    path('', home),  # Home view for the root URL
-]
